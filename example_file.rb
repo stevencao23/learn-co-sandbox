f2508e4b82d345024dc8e7a966db07a9745458ac
steven cao <stevencao23@gmail.com>
2020-12-18 00:01:40 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,3 @@
+famous_cats = ["lil bub", "grumpy cat", "Maru"]
+
+puts famous_cats.index("grumpy cat")
---
+++ test.rb
@@ -0,0 +1,7 @@
+counter = 0
+pets = ["dog", "cat", "fish", "bird", "hamster"]
+
+while pets[counter] do 
+  puts pets[counter]
+  counter += 1
+end
